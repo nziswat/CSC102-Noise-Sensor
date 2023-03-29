c1bbb27dd2857df16b7dd34a1cde6f05caeaf03f
KevinPanasiuk <kevin.panasiuk@spartans.ut.edu>
2023-03-29 14:59:44 -0400

    Start of the audio sampler
    Signed-off-by: KevinPanasiuk <kevin.panasiuk@spartans.ut.edu>

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 
+### The main code for the microphone and GPIO interface
+import RPi.GPIO as GPIO
+
+###Set GPIOs slots below
+VolIn = 24 #VOLume IN. Inputted from the ADC
+
+
+def AudioSampler(): #Samples the last last 
+    
