378c9cad4ab17090dc4ce139aa0217205dbee74a
nziswat <kevinpanasiuk@gmail.com>
2023-05-03 11:01:04 -0400

    Updated some comments

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 ############
 ### The main code for the microphone and GPIO interface
 
-
+#As you can see, a lot of modules are required to get this thing running.
 import RPi.GPIO as GPIO
 import busio
 import digitalio
@@ -42,7 +42,7 @@ chan2 = AnalogIn(mcp, MCP.P1)
 
 
 
-class MainSystem():
+class MainSystem(): #The infraction system works as an object to make it easier to access the variables. 
 
     infractions = 0
     lastsecond = []
@@ -54,7 +54,7 @@ class MainSystem():
     def AudioSampler(self): #Samples the audio and proceeds to integrate the last second or so.
         return (sum(self.lastsecond)/len(self.lastsecond))
 
-    def noiseMaker():
+    def noiseMaker(): #Beeps a few times to warn of a high noise. 
         GPIO.output(beeper, GPIO.HIGH)
         time.sleep(0.25)
         GPIO.output(beeper, GPIO.LOW)
@@ -73,7 +73,7 @@ class MainSystem():
 
 
 
-    def noiseMakerSuper():
+    def noiseMakerSuper(): #A more annoying version of the above function.
         GPIO.output(beeper, GPIO.HIGH)
         time.sleep(3)
         GPIO.output(beeper, GPIO.LOW)
@@ -81,12 +81,13 @@ class MainSystem():
 
 
         
-    def Infraction(self): #TODO, keeps a tally of the amount of infractions that has accumulated. 
+    def Infraction(self): #The function used to increment the Infractions and resets it as well. 
         self.infractions += 1
         if self.infractions == 3:
-            self.noiseMaker()
+            self.noiseMaker() #
         if self.infractions == 5:
             self.noiseMakerSuper()
+            self.infractions = 0
             
             
 
@@ -95,11 +96,12 @@ class MainSystem():
 System = MainSystem() #Make the Counter Object
     
             
-while True:
+while True: #can be considered the 'main' part of the program
 
+    #Every 'frame', add the recorded value to the list of recorded values.
     System.lastsecond.append(chan.value)
     
-    if len(System.lastsecond) > 1000:
+    if len(System.lastsecond) > 1000: #And then every 1000 'frames' find the average and compare to the threshold
         audioCheck = System.AudioSampler()
         print (audioCheck)
         if audioCheck > 5000:
